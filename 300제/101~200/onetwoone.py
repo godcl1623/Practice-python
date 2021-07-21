e66f83b399d1da0ae81a80ef9c6cad765772e675
godcl1623 <delphi5281@gmail.com>
2021-07-21 22:47:10 +0900

    파이썬 복습 101~150(121~130 제외)

--- 300제/101~200/onetwoone.py
+++ 300제/101~200/onetwoone.py
@@ -196,17 +196,17 @@ closing_price	최근 24시간 내 마지막 거래금액
 min_price	최근 24시간 내 최저 거래금액
 max_price	최근 24시간 내 최고 거래금액
 '''
-import requests
-btc = requests.get("https://api.bithumb.com/public/ticker/").json()['data']
-시가 = int(btc['opening_price'])
-종가 = int(btc['closing_price'])
-최고가 = int(btc['max_price'])
-최저가 = int(btc['min_price'])
-변동폭 = 최고가 - 최저가
-if (시가 + 변동폭) > 최고가:
-    print('상승장')
-else:
-    print('하락장')
+# import requests
+# btc = requests.get("https://api.bithumb.com/public/ticker/").json()['data']
+# 시가 = int(btc['opening_price'])
+# 종가 = int(btc['closing_price'])
+# 최고가 = int(btc['max_price'])
+# 최저가 = int(btc['min_price'])
+# 변동폭 = 최고가 - 최저가
+# if (시가 + 변동폭) > 최고가:
+#     print('상승장')
+# else:
+#     print('하락장')
 
 
 
