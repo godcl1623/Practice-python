b5079d364ac54ad5492428c89bf7022bdf7291b5
godcl1623 <delphi5281@gmail.com>
2021-08-02 23:19:22 +0900

    파이썬 문제풀이 - 241~250 풀이

--- 300제/201~300/twofourone.py
+++ 300제/201~300/twofourone.py
@@ -1,6 +1,7 @@
 '''
-오답현황(~210727)
-풀이 보고 다시 풀어보기
+오답현황(~210802)
+풀이 보고 다시 풀어보기 * 2
+241, 242, 248 제외
 '''
 '''
 241 현재시간
@@ -21,34 +22,50 @@ datetime 모듈의 timedelta를 사용해서 오늘로부터 5일, 4일, 3일, 2
 '''
 
 '''
-244 strftime
+244 strftime(못풂)
 현재시간을 얻어온 후 다음과 같은 포맷으로 시간을 출력해보세요. strftime 메서드를 사용하세요.
 
 18:35:01 
 '''
 
 '''
-245 strptime
+245 strptime(못풂)
 datetime.datetime.strptime 메서드를 사용하면 문자열 형식의 시간을 datetime.datetime 타입의 시간 값으로 만들어줍니다. "2020-05-04"의 문자열을 시간 타입으로 변환해보세요.
 '''
+print(datetime.datetime.strptime('2020-05-04', '%Y-%m-%d'));
 
 '''
 246 sleep 함수
 time 모듈, datetime 모듈을 사용해서 1초에 한 번 현재 시간을 출력하는 코드를 작성하세요.
 '''
+"""
+import time;
+import datetime;
+while 1 > 0:
+    print(datetime.datetime.now());
+    time.sleep(1);
+"""
 
 '''
 247 모듈 임포트
 모듈을 임포트하는 4가지 방식에 대해 설명해보세요.
 '''
+"""
+1. import 모듈
+2. from 모듈 import 메소드
+3. from 모듈 import *
+4. import 모듈 as 식별자
+"""
 
 '''
 248 os 모듈
 os 모듈의 getcwd 함수를 호출하여 현재 디렉터리의 경로를 화면에 출력해보세요.
 '''
+import os;
+print(os.getcwd());
 
 '''
-249 rename 함수
+249 rename 함수(못풂)
 바탕화면에 텍스트 파일을 하나 생성한 후 os 모듈의 rename 함수를 호출하여 해당 파일의 이름을 변경해보세요.
 '''
 
