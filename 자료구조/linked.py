8f9782428ac222bb7db393445e1d41e49bd4b011
godcl1623 <delphi5281@gmail.com>
2021-08-15 01:40:12 +0900

    자료구조 복습 - 더블 링크드 리스트 복습 진행중 - 삭제 이후부터 다시

---
+++ 자료구조/linked.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, data, prev=None, next=None):
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+class NodeMgmt:
+    def __init__(self, data):
+        self.head = Node(data)
+        self.tail = self.head
+
+    def insert(self, data):
+        if self.head == '':
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            node = self.head
+            while node.next:
+                node = node.next
+            new = Node(data)
+            node.next = new
+            new.prev = node
+            self.tail = new
+
+    def desc(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
+
+    def delete(self, data):
+        if self.head == '':
+            print('데이터가 없습니다')
+
+        if self.head.data == data:
+            temp = self.head
+            self.head = self.head.next
+            del temp
+        else:
+            node = self.head
+            while node.next:
+                if node.next.data == data:
+                    temp = node.next
+                    node.next = node.next.next
+                    del temp
+                    pass
+                else:
+                    node = node.next
+
+doublink = NodeMgmt(0);
+for index in range(1, 10):
+    doublink.insert(index)
+doublink.delete(9)
+doublink.desc();
+# print(doublink.tail.data)
