08064af4e2b2fb343e1f171bb0abfa1d6aa3a285
godcl1623 <delphi5281@gmail.com>
2021-09-03 23:03:03 +0900

    알고리즘 강의 복습 - 삽입 정렬

--- 알고리즘/basic_sort.py
+++ 알고리즘/basic_sort.py
@@ -9,17 +9,27 @@
 #       break
 #   return array
 
-def select_sort(array):
+# def select_sort(array):
+#   for cycle in range(len(array) - 1):
+#     lowest = cycle
+#     for index in range(cycle, len(array)):
+#       if array[index] < array[lowest]:
+#         lowest = index
+#     array[cycle], array[lowest] = array[lowest], array[cycle]
+#   return array
+
+def insert_sort(array):
   for cycle in range(len(array) - 1):
-    lowest = cycle
-    for index in range(cycle, len(array)):
-      if array[index] < array[lowest]:
-        lowest = index
-    array[cycle], array[lowest] = array[lowest], array[cycle]
+    for index in range(cycle+1, 0, -1):
+      if array[index] < array[index - 1]:
+        array[index], array[index - 1] = array[index - 1], array[index]
+      else:
+        break
   return array
 
 import random;
 
 rand_array = random.sample(range(100), 50)
 # print(bubble_sort(rand_array));
-print(select_sort(rand_array));
+# print(select_sort(rand_array));
+print(insert_sort(rand_array));
