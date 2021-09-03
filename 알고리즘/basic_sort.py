7930301323741d06a491ace2c6cd2707e52f7cd6
godcl1623 <delphi5281@gmail.com>
2021-09-03 22:40:31 +0900

    알고리즘 강의 복습 - 버블 정렬, 선택 정렬 복습

---
+++ 알고리즘/basic_sort.py
@@ -0,0 +1,25 @@
+# def bubble_sort(array):
+#   for cycle in range(len(array) - 1):
+#     swap = False
+#     for index in range(len(array) - cycle - 1):
+#       if array[index] > array[index + 1]:
+#         array[index], array[index + 1] = array[index + 1], array[index]
+#         swap = True
+#     if swap == False:
+#       break
+#   return array
+
+def select_sort(array):
+  for cycle in range(len(array) - 1):
+    lowest = cycle
+    for index in range(cycle, len(array)):
+      if array[index] < array[lowest]:
+        lowest = index
+    array[cycle], array[lowest] = array[lowest], array[cycle]
+  return array
+
+import random;
+
+rand_array = random.sample(range(100), 50)
+# print(bubble_sort(rand_array));
+print(select_sort(rand_array));
