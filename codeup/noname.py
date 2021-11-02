b1b248b39d6e24064a45c3d87bbda6a7e441e891
godcl1623 <delphi5281@gmail.com>
2021-11-02 23:28:26 +0900

    알고리즘 문제풀이 - 코드업 복습 2회차

--- codeup/noname.py
+++ codeup/noname.py
@@ -1,36 +1,24 @@
 '''
 1번 통과한 목록(코드업 파이썬 100제) ※이하 통과 = 모범 답안과 유사한 방법으로 풀이
-88, 91, 94, 95, 96
+8, 18, 24, 42, 46, 47, 52, 53, 58, 60, 61, 62, 64, 74, 79, 81, 84, 88, 91, 94, 95, 96
 '''
 '''
 2번 통과한 목록(코드업 파이썬 100제)
+6, 7, 17, 20, 25, 26, 27, 28, 29, 30, 31, 33, 38, 48, 54, 70, 75, 76, 78, 82, 85
 '''
 '''
 3번 통과한 목록(코드업 파이썬 100제)
 '''
 '''
 힌트 목록
+18: f-string 사용하지 말고 풀어보기
+24: f-string, sep 제외
+42, 44: :.*f 제외
+52, 53: 부등호 제외
+63: 두 수 중 후자가 큰 경우를 제외하면 모두 전자가 표시되도록 작성
+81: 3줄로 작성
 97: O(n^2) 이내로 작성
 '''
-m = []
-for _ in range(10):
-  m.append(list(map(int, input().split())))
-br = 1
-for i in range(len(m)):
-  if i == 0 or i == 9:
-    continue
-  for j in range(br, len(m[i])):
-    if m[i][j] == 0:
-      m[i][j] = 9
-    elif m[i][j] == 1:
-      if br != 9:
-        br = j - 1
-        break
-    else:
-      m[i][j] = 9
-      br = 9
-      break
-for i in range(len(m)):
-  for j in m[i]:
-    print(j, end=' ')
-  print()
+w, h, b = map(int, input().split())
+# a = int(input())
+print(f'{w*h*b/8/1024/1024:.2f} MB')
