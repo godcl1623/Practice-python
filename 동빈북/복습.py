c1d9241a2895ed37577537c82e8c1455625fc316
godcl1623 <delphi5281@gmail.com>
2021-11-17 00:55:33 +0900

    알고리즘 문제풀이 - 백준 시뮬레이션 문제풀이

--- 동빈북/복습.py
+++ 동빈북/복습.py
@@ -46,42 +46,7 @@ print(count)
 import time
 start_time = time.time()
 ############################################################
-n, m = map(int, input().split())
-a, b, d = map(int, input().split())
-field = [list(map(int, input().split())) for i in range(n)]
-field_rec = [[0]*m for i in range(n)]
-field_rec[a][b] = 1
-da = [-1, 0, 1, 0]
-db = [0, 1, 0, -1]
-def turn_left():
-  global d
-  d -= 1
-  if d == -1:
-    d = 3
-count = 1
-turn_time = 0
-while True:
-  turn_left()
-  na = a + da[d]
-  nb = b + db[d]
-  if field_rec[na][nb] == 0 and field[na][nb] == 0:
-    field_rec[na][nb] = 1
-    a = na
-    b = nb
-    count += 1
-    turn_time = 0
-    continue
-  else:
-    turn_time += 1
-  if turn_time == 4:
-    na = a - da[d]
-    nb = b - db[d]
-    if field[na][nb] == 0:
-      a, b = na, nb
-    else:
-      break
-    turn_time = 0
-print(count)
+
 ############################################################
 end_time = time.time()
 print('time: ', end_time - start_time)
--- 백준/구현.py
+++ 백준/구현.py
@@ -2,6 +2,8 @@ import time
 '''
 구현, 시뮬레이션, 브루트 포스 모두 같이 풀기
 '''
+"""
+시뮬레이션
 '''
 정답
 #1966. 프린터 큐
@@ -28,108 +30,53 @@ for _ in range(ints):
       q_curr_idx.append(q_curr_idx.pop(0))
       continue
 '''
-start_time = time.time()
-############################################################
+'''
+# 14891 톱니바퀴
 n, m = map(int, input().split())
 r, c, d = map(int, input().split())
-room = [list(map(int, input().split())) for i in range(n)]
-clean_count = 0
-turn_count = 0
+curr_map = [list(map(int, input().split())) for i in range(n)]
+map_clean = [[0] * m for i in range(n)]
+map_clean[r][c] = 1
+rPoss = [-1, 0, 1, 0]
+cPoss = [0, 1, 0, -1]
 def turn_left():
-  global d, turn_count
-  turn_count += 1
-  if d == 0:
+  global d
+  d -= 1
+  if d == -1:
     d = 3
-  else:
-    d -= 1
+cleaned = 1
+turn_count = 0
 while True:
-  print('(r, c): ', (r, c))
-  print('clean stat: ', room[r][c])
-  print('d: ', d)
-  print('turn_count: ', turn_count)
-  # 사방 탐색이 진행중인 경우
-  if turn_count < 4:
-    # 현재 위치가 청소가 안 된 상태인 경우
-    if room[r][c] == 0:
-      room[r][c] = 1
-      clean_count += 1
-      print('##### cleaned #####')
-      for i in range(len(room)):
-        for j in range(len(room[i])):
-          if i == r and j == c:
-            print(room[i][j]+1, end=' ')
-          else:
-            print(room[i][j], end=' ')
-        print()
-    # 현재 위치가 청소가 된 상태인 경우 -> 탐색 실행
-    else:
-      # 현재 위치 왼쪽이 청소가 안 된 공간인 경우 -> 좌회전 후 이동
-      if d == 0:
-        print('current: north')
-        if room[r][c-1] == 0:
-          print('turn and go to west')
-          turn_left()
-          turn_count = 0
-          c -= 1
-          continue
-        else:
-          print('turn to west')
-          turn_left()
-          continue
-      elif d == 1:
-        print('current: east')
-        if room[r-1][c] == 0:
-          print('turn and go to north')
-          turn_left()
-          turn_count = 0
-          r -= 1
-          continue
-        else:
-          print('turn to north')
-          turn_left()
-          continue
-      elif d == 2:
-        print('current: south')
-        if room[r][c+1] == 0:
-          print('turn and go to east')
-          turn_left()
-          turn_count = 0
-          c += 1
-          continue
-        else:
-          print('turn to east')
-          turn_left()
-          continue
-      else:
-        print('current: west')
-        if room[r+1][c] == 0:
-          print('turn and go to south')
-          turn_left()
-          turn_count = 0
-          r += 1
-          continue
-        else:
-          print('turn to south')
-          turn_left()
-          continue
-  # 사방 탐색을 진행한 결과 네 방향 모두가 청소가 이미 끝났거나 벽인 경우
+  turn_left()
+  new_r = r + rPoss[d]
+  new_c = c + cPoss[d]
+  if map_clean[new_r][new_c] == 0 and curr_map[new_r][new_c] == 0:
+    map_clean[new_r][new_c] = 1
+    r, c = new_r, new_c
+    cleaned += 1
+    turn_count = 0
+    continue
   else:
-    if d == 0:
-      r += 1
-      turn_count = 0
-      continue
-    elif d == 1:
-      c -= 1
-      turn_count = 0
-      continue
-    elif d == 2:
-      r -= 1
-      turn_count = 0
-      continue
+    turn_count += 1
+  if turn_count == 4:
+    new_r = r - rPoss[d]
+    new_c = c - cPoss[d]
+    if curr_map[new_r][new_c] == 0:
+      r, c = new_r, new_c
     else:
-      c += 1
-      turn_count = 0
-      continue
+      break
+    turn_count = 0
+print(cleaned)
+'''
+"""
+start_time = time.time()
+############################################################
+# 14891 톱니바퀴 - 못 맞힘
+
+# 맞닿은 톱니의 극이 다르다 -> 한 톱니를 회전시키면 극이 다른 톱니가 반대 방향으로 회전
+# 맞닿은 톱니의 극이 같다 -> 한 톱니를 회전시키면 극이 같은 톱니는 그대로 있음
+# 1, 4번 톱니: 인덱스 2, 인덱스 6이 맞닿음
+# 2, 3번 톱니: 인덱스 2와 6이 맞닿음
 ############################################################
 end_time = time.time()
 print('time: ', end_time - start_time)
