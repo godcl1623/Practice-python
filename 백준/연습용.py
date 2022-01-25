d22e436821e8d53a796a4c2fed5d53404776cafb
godcl1623 <delphi5281@gmail.com>
2022-01-25 23:31:59 +0900

    백준 구현 복습중

--- 백준/연습용.py
+++ 백준/연습용.py
@@ -3,13 +3,32 @@
 11399(○), 11047(○), 1541(△)
 
 # 구현
-1966, 14891
+1966(△), 14891
 '''
-original_formula = input().split('-')
-result = 0
-for i in original_formula[0].split('+'):
-	result += int(i)
-for i in original_formula[1:]:
-	for j in i.split('+'):
-		result -= int(j)
-print(result)
+'''
+# 1966: 로직 자체는 맞는데, remove 대신 pop을 써야함 + docs는 필요 없음
+cases = int(input())
+for _ in range(cases):
+	num_of_docs, curr_doc_idx = map(int, input().split())
+	docs = [i for i in range(num_of_docs)]
+	prios = list(map(int, input().split()))
+	curr_prio = prios[curr_doc_idx]
+	curr_prio_idx = prios.index(curr_prio)
+	order = 1
+	while True:
+		if prios[0] == max(prios):
+			if prios[0] == curr_prio and docs[0] == curr_prio_idx:
+				break
+			else:
+				prios.remove(prios[0])
+				docs.remove(docs[0])
+				order += 1
+		else:
+			reorder_docs = docs[0]
+			docs.remove(reorder_docs)
+			docs.append(reorder_docs)
+			reorder_prios = prios[0]
+			prios.remove(reorder_prios)
+			prios.append(reorder_prios)
+	print(order)
+	'''
