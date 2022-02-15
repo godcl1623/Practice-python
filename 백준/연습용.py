cb74c1ef370ed9eb865ef6e5120c944ed8b88605
godcl1623 <delphi5281@gmail.com>
2022-02-15 19:48:08 +0900

    백준 - 해쉬, 트리 문제풀이

--- 백준/연습용.py
+++ 백준/연습용.py
@@ -12,75 +12,130 @@
 # 스택
 3986(O)
 # 링크드 리스트(= 연결 리스트)
-2605(△) - 참조: https://heewon9809.tistory.com/264
+2605(△)
 # 해쉬
+9375(X) - 참조: https://hongcoding.tistory.com/60
 # 트리
+14425(X) - 참조: https://jinho-study.tistory.com/980
 # 힙
 '''
 '''
-# 10845
-import sys
-cmds_num = int(sys.stdin.readline())
-q = []
-for i in range(cmds_num):
-	cmd_input = sys.stdin.readline().split()
-	if cmd_input[0] == 'push':
-		q.append(int(cmd_input[1]))
-	elif cmd_input[0] == 'pop':
-		if len(q) == 0:
-			print(-1)
+# 9375(답안)
+t = int(input())
+
+for i in range(t):
+	n = int(input())
+	weardict = {}
+	for j in range(n):
+		wear = list(input().split())
+		if wear[1] in weardict:
+			weardict[wear[1]].append(wear[0])
 		else:
-			temp = q[0]
-			del q[0]
-			print(temp)
-	elif cmd_input[0] == 'size':
-		print(len(q))
-	elif cmd_input[0] == 'empty':
-		if len(q) == 0:
-			print(1)
-		else:
-			print(0)
-	elif cmd_input[0] == 'front':
-		if len(q) == 0:
-			print(-1)
-		else:
-			print(q[0])
-	elif cmd_input[0] == 'back':
-		if len(q) == 0:
-			print(-1)
-		else:
-			print(q[-1])
+			weardict[wear[1]] = [wear[0]]
+	print(weardict)
+
+	cnt = 1 # 각 종류마다 항목의 개수
+
+	for k in weardict:
+		cnt *= (len(weardict[k])+1)
+	print(cnt-1)
 '''
 '''
-# 3986
-import sys
-words_num = int(sys.stdin.readline())
+# 14425(오답)
+n, m = map(int, input().split())
 count = 0
-for _ in range(words_num):
-	word = sys.stdin.readline().rstrip()
-	stk = []
-	for i in word:
-		if len(stk) == 0:
-			stk.append(i)
-		else:
-			if stk[-1] == i:
-				del stk[-1]
+
+class Node:
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
+
+class Tree:
+	def __init__(self, head):
+		self.head = head
+
+	def save(self, value):
+		self.current = self.head
+		while True:
+			if len(value) < len(self.current.value):
+				if self.current.left != None:
+					self.current = self.current.left
+				else:
+					self.current.left = Node(value)
+					return
+			elif len(value) > len(self.current.value):
+				if self.current.right != None:
+					self.current = self.current.right
+				else:
+					self.current.right = Node(value)
+					return
 			else:
-				stk.append(i)
-	if len(stk) == 0:
-		count += 1
+				idx = 0
+				while True:
+					if ord(value[idx]) == ord(self.current.value[idx]):
+						idx += 1
+					elif ord(value[idx]) < ord(self.current.value[idx]):
+						idx = 0
+						if self.current.left != None:
+							self.current = self.current.left
+						else:
+							self.current.left = Node(value)
+							return
+					else:
+						idx = 0
+						if self.current.right != None:
+							self.current = self.current.right
+						else:
+							self.current.right = Node(value)
+							return
+
+	def search(self, value):
+		global count
+		self.current = self.head
+		while self.current:
+			if value == self.current.value:
+				count += 1
+				return True
+			else:
+				if len(value) < len(self.current.value):
+					self.current = self.current.left
+				elif len(value) > len(self.current.value):
+					self.current = self.current.right
+				else:
+					idx = 0
+					while self.current:
+						if value == self.current.value:
+							count += 1
+							return True
+						if ord(value[idx]) == ord(self.current.value[idx]):
+							idx += 1
+						elif ord(value[idx]) < ord(self.current.value[idx]):
+							idx = 0
+							self.current = self.current.left
+						else:
+							idx = 0
+							self.current = self.current.right
+		return False
+
+root_node = Node(input())
+tree = Tree(root_node)
+
+for _ in range(n - 1):
+	tree.save(input())
+
+for _ in range(m):
+	tree.search(input())
+
 print(count)
 '''
-n = int(input())
-cards = list(map(int, input().split()))
-students = [i + 1 for i in range(n)]
-order = {}
-curr_idx = 0
-while len(order.keys()) != len(students):
-	order[curr_idx] = [curr_idx - cards[curr_idx], students[curr_idx]]
-	curr_idx += 1
-new_list = []
-for i in order.values():
-	new_list.insert(i[0], i[1])
-for i in new_list:
-	print(i, end=' ')
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+s = set([input().rstrip() for _ in range(n)])
+cnt = 0
+for _ in range(m):
+	t = input().rstrip()
+	if t in s:
+		cnt += 1
+print(cnt)
